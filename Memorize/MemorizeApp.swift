6808a0b9c397ab5eb38994f45966c2f2aa527f4e
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2024-04-09 17:11:48 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kiran Boyle on 09.04.24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
