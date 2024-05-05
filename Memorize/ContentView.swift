9a418a14d0369d66aa2e63e193c1f5d88fbc7ff5
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2024-05-05 20:34:19 +0200

    alter view

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack(content: {
+            Rectangle()
+            Text("👻").font(.largeTitle)
+        })
+        .padding(12)
+        .foregroundColor(.orange)
     }
 }
 
