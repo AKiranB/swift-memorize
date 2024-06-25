123f22cc457a02256950a604900aa20dbd2841fc
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2024-06-25 12:34:09 +0200

    add face up cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,20 +1,31 @@
-//
-//  ContentView.swift
-//  Memorize
-//
-//  Created by Kiran Boyle on 09.04.24.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+
+struct ContentView:View {
     var body: some View {
-        ZStack(content: {
-            Rectangle()
-            Text("👻").font(.largeTitle)
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+     
+        }
+    }
+}
+
+
+struct CardView: View {
+    var isFaceUp:Bool = false
+    var body: some View {
+        ZStack(content:{
+            if isFaceUp {
+                RoundedRectangle(cornerRadius:12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius:12)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius:12)
+            }
         })
-        .padding(12)
-        .foregroundColor(.orange)
     }
 }
 
