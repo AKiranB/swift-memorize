e9b6e1c15777b5809ae7bc42d660ccae3e5db4e1
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2024-06-26 14:23:54 +0200

    updates

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,31 +1,37 @@
 import SwiftUI
 
 
-struct ContentView:View {
+struct ContentView: View {
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
             CardView()
             CardView()
             CardView()
-     
+            CardView()
         }
+        .foregroundColor(.orange)
+        .padding()
     }
 }
 
 
 struct CardView: View {
-    var isFaceUp:Bool = false
+    @State private var isFaceUp: Bool = false
+    
+    let base = RoundedRectangle(cornerRadius:12)
+    
     var body: some View {
-        ZStack(content:{
+        ZStack {
             if isFaceUp {
-                RoundedRectangle(cornerRadius:12).foregroundColor(.white)
-                RoundedRectangle(cornerRadius:12)
+                base.fill(.white)
+                base.strokeBorder(lineWidth:2)
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius:12)
+                base.fill()         
             }
-        })
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
