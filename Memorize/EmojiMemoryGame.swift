56462f85c07d132d060a3eb060996c3dec25120c
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2024-11-07 17:51:17 +0100

    fix stuff

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 
 class EmojiMemoryGame {
-    private var model: MemoryGame<String> = MemoryGame(numberOfPairsOfCards: 5)
-    
-    var cards: Array<MemoryGame<String>.Card>{
+    private var model: MemoryGame<String> = MemoryGame(numberOfPairsOfCards: 5) { index in
+        ["Card 1", "Card 2", "Card 3"][index]
+    }
+
+    var cards: Array<MemoryGame<String>.Card> {
         return model.cards
     }
     
-    func choose(card: MemoryGame <String>.Card){
+    func choose(card: MemoryGame<String>.Card) {
         model.choose(card: card)
     }
-    
 }
